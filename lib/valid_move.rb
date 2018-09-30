3e46557c7991327aa01e3f4ad6d59a6c6514a992
beneficial-bundle-8122 <satyasinha94@gmail.com>
2018-09-30 19:28:39 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,8 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if index.between?(0, 8) && !position_taken(board, index)
+  if index.between?(0, 8) 
     return false
-  else 
+  else
     return true
   end
 end
